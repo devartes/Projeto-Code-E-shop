865570ea06aa243395a4eef2dd191e2d38aca4eb
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2022-04-17 20:49:12 -0300

    removendo alert

--- api/login.php
+++ api/login.php
@@ -586,31 +586,6 @@ session_start();
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     <!-- ***************************************** -->
 
-    <!-- *********** POP-UP ********* -->
-
-    <script src="js/sweetalert.min.js"></script>
-
-
-    <?php
-    // PHP program to pop an alert
-    // message box on the screen
-
-    // Function defnition
-    function function_alert($message)
-    {
-
-        // Display the alert box 
-        echo "<script>alert('$message');</script>";
-    }
-
-
-    // Function call
-    function_alert("Isso é um teste!");
-
-    ?>
-
-    <!-- **************************** -->
-
     <!-- FOOTER -->
     <footer>
     
